9b5846defb478028da7f2b2e7908ca18cee92043
Ben Sigmon <bcsigmon@ua.edu>
2015-10-16 11:04:43 -0500

    Added week 2 package.

---
+++ src/week2/Week2.java
@@ -0,0 +1,34 @@
+package week2;
+
+public class Week2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Sizeof byte is: " + Byte.SIZE);
+		System.out.println("Max byte is: " + Byte.MAX_VALUE);
+		System.out.println("Min byte is: " + Byte.MIN_VALUE);
+		System.out.println();
+		
+		System.out.println("Sizeof short is: " + Short.SIZE);
+		System.out.println("Max short is: " + Short.MAX_VALUE);
+		System.out.println("Min short is: " + Short.MIN_VALUE);
+		System.out.println();
+		
+		System.out.println("Sizeof int is: " + Integer.SIZE);
+		System.out.println("Max int is: " + Integer.MAX_VALUE);
+		System.out.println("Min int is: " + Integer.MIN_VALUE);
+		System.out.println();
+		
+		System.out.println("Sizeof float is: " + Float.SIZE);
+		System.out.println("Max float is: " + Float.MAX_VALUE);
+		System.out.println("Min float is: " + Float.MIN_VALUE);
+		System.out.println();
+		
+		System.out.println("Sizeof double is: " + Double.SIZE);
+		System.out.println("Max double is: " + Double.MAX_VALUE);
+		System.out.println("Min double is: " + Double.MIN_VALUE);
+		System.out.println();
+
+	}
+
+}
